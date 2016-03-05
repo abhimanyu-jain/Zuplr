05e1f2f32f9e3e05ed0e182c09744c722b78aea1
Mahendhar <mahendarreddy@live.com>
2016-03-05 23:57:59 +0530

    Validating user phonenumber through regex

--- app/models/userprofile.rb
+++ app/models/userprofile.rb
@@ -1,4 +1,5 @@
 class Userprofile < ActiveRecord::Base
 	has_one :user, :dependent => :destroy
   validates_presence_of :city, :phonenumber
+  # validates_format_of :phonenumber, :with => /((\+*)((0[ -]+)*|(91 )*)(\d{12}+|\d{10}+))|\d{5}([- ]*)\d{6}/
 end
