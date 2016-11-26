82968c8d150ceb429a7af0d1b18e4d5614361d8f
Abhimanyu Jain <abhimanyujain2k6@gmail.com>
2016-11-26 19:40:19 +0530

    item category feature added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126135451) do
+ActiveRecord::Schema.define(version: 20161126140324) do
 
   create_table "brands", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -59,6 +59,12 @@ ActiveRecord::Schema.define(version: 20161126135451) do
   end
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
+
+  create_table "item_categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "item_statuses", force: :cascade do |t|
     t.string   "name",       limit: 255
